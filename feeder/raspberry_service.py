e30d92658d414f856e2250df8fe28c40b9b442f4
ckma23 <curtiskma@gmail.com>
2019-05-07 00:07:09 -0700

    Added servo motor control

--- feeder/raspberry_service.py
+++ feeder/raspberry_service.py
@@ -21,3 +21,30 @@ class LedLighter:
             GPIO.output(self.pin_number,GPIO.LOW)
             time.sleep(1)
             counter += 1
+
+        GPIO.cleanup()
+
+class FeederMotor:
+    def __init__(self,pin_number):
+        self.pin_number = pin_number
+
+    def setup_motor(self):
+        print ("setting up motor")
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin_number, GPIO.OUT)
+        p = GPIO.PWM(self.pin_number,50)
+
+    def motor_move(self,number_of_times):
+        counter = 0
+        while counter < number_of_times:
+            p.start(7.5)
+            p.ChangeDutyCycle(2.5)
+            time.sleep(1)
+            p.ChangeDutyCycle(2.5)
+            time.sleep(1)
+            p.ChangeDutyCycle(2.5)
+            time.sleep(1)
+            p.stop()
+            time.sleep(1)
+            counter += 1
+        GPIO.cleanup()
--- feeder/views.py
+++ feeder/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from feeder.raspberry_service import LedLighter as LedLighter
+from feeder.raspberry_service import FeederMotor as FeederMotor
 
 class FeederView:
     def index(request):
         page_body = CatsMessage()
         page_body.populate()
-
         if request.method == 'GET':
             return render(request,'feeder/index.html', page_body.context)
 
-        elif request.method == 'POST':
+        elif 'light' in request.POST.keys():
+            print ("LIGHT")
             light_up(request)
+            return render(request,'feeder/index.html', page_body.context)
+
+        elif 'feed' in request.POST.keys():
+            print ("FEED")
+            feed_us(request)
             return render(request,'feeder/index.html', page_body.context)
 
 class CatsMessage:
@@ -22,8 +28,14 @@ class CatsMessage:
         self.context["cat_2"] = "Tuna"
 
 def light_up(request):
-    print("FEEDING")
+    print("LIGHTING")
     ll_handler = LedLighter(18)
     ll_handler.setup_led()
     ll_handler.light_up(5)
+
+def feed_us(request):
+    print ("FEEDING")
+    fm_handler = FeederMotor(32)
+    fm_handler.setup_motor()
+    fm_handler.motor_move()
 # Create your views here.
